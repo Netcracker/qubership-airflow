92da3f3e781af14b0b99002944b934c9ebdeb0c6
Aakcht <aakcht@gmail.com>
2025-10-03 15:43:07 +0300

    Add test s3dagbundlepackage

--- docker/s3dagbundlepackage/qss3dagbundle/qs_s3_dag_bundle.py
+++ docker/s3dagbundlepackage/qss3dagbundle/qs_s3_dag_bundle.py
@@ -2,7 +2,7 @@ import subprocess
 
 from airflow.providers.amazon.aws.bundles.s3 import S3DagBundle
 
-checksum_command = 'find /tmp/airflow/dag_bundles -type f -name "*.yaml" -or -name "*.py" -or -name "*.json" -or -name "*.csv" -or -name "*.zip" -or -name "*.sql" | sort -u | xargs cat | md5sum' # noqa: E501
+checksum_command = 'find /tmp/airflow/dag_bundles -type f -name "*.yaml" -or -name "*.py" -or -name "*.json" -or -name "*.csv" -or -name "*.zip" -or -name "*.sql" | sort -u | xargs cat | md5sum'  # noqa: E501
 
 
 class QSS3DAGBundle(S3DagBundle):
