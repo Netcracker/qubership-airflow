59b863e937490dd1621bc6cbc43a2d3c14b68f0e
Aakcht <aakcht@gmail.com>
2025-10-03 15:09:05 +0300

    Add test s3dagbundlepackage

---
+++ docker/s3dagbundlepackage/qss3dagbundle/qs_s3_dag_bundle.py
@@ -0,0 +1,12 @@
+import subprocess
+
+from airflow.providers.amazon.aws.bundles.s3 import S3DagBundle
+
+checksum_command = 'find /tmp/airflow/dag_bundles -type f -name "*.yaml" -or -name "*.py" -or -name "*.json" -or -name "*.csv" -or -name "*.zip" -or -name "*.sql" | sort -u | xargs cat | md5sum'
+
+class QSS3DAGBundle(S3DagBundle):
+    supports_versioning = True
+
+    def get_current_version(self) -> str | None:
+        checksum = subprocess.run(checksum_command, shell=True, capture_output=True, text=True, check=True)
+        return checksum.stdout
