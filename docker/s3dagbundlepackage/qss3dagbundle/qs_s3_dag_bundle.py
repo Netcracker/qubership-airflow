16ab809fa3426d728a0731efa5a24d9a4c2bbf4c
Aakcht <aakcht@gmail.com>
2025-10-06 12:37:15 +0500

    Update qs_s3_dag_bundle.py

--- docker/s3dagbundlepackage/qss3dagbundle/qs_s3_dag_bundle.py
+++ docker/s3dagbundlepackage/qss3dagbundle/qs_s3_dag_bundle.py
@@ -13,3 +13,19 @@ class QSS3DAGBundle(S3DagBundle):
             checksum_command, shell=True, capture_output=True, text=True, check=True
         )
         return checksum.stdout
+
+    def view_url_template(self) -> str | None:
+        if hasattr(self, "_view_url_template") and self._view_url_template:
+            # Because we use this method in the view_url method, we need to handle
+            # backward compatibility for Airflow versions that doesn't have the
+            # _view_url_template attribute. Should be removed when we drop support for Airflow 3.0
+            return self._view_url_template
+        # https://<bucket-name>.s3.<region>.amazonaws.com/<object-key>
+        url = f"https://{self.bucket_name}.s3"
+        if self.s3_hook.region_name:
+            url += f".{self.s3_hook.region_name}"
+        url += ".amazonaws.com"
+        if self.prefix:
+            url += f"/{self.prefix}"
+
+        return url
