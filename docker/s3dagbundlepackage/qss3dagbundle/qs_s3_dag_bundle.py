5c1baec17a32ac71febec67b2d3c9b4d15b74ba3
Aakcht <aakcht@gmail.com>
2025-10-07 10:47:32 +0300

    Add test s3dagbundlepackage

--- docker/s3dagbundlepackage/qss3dagbundle/qs_s3_dag_bundle.py
+++ docker/s3dagbundlepackage/qss3dagbundle/qs_s3_dag_bundle.py
@@ -6,26 +6,4 @@ checksum_command = 'find /tmp/airflow/dag_bundles -type f -name "*.yaml" -or -na
 
 
 class QSS3DAGBundle(S3DagBundle):
-    supports_versioning = True
-
-    def get_current_version(self) -> str | None:
-        checksum = subprocess.run(
-            checksum_command, shell=True, capture_output=True, text=True, check=True
-        )
-        return checksum.stdout
-
-    def view_url_template(self) -> str | None:
-        if hasattr(self, "_view_url_template") and self._view_url_template:
-            # Because we use this method in the view_url method, we need to handle
-            # backward compatibility for Airflow versions that doesn't have the
-            # _view_url_template attribute. Should be removed when we drop support for Airflow 3.0
-            return self._view_url_template
-        # https://<bucket-name>.s3.<region>.amazonaws.com/<object-key>
-        url = f"https://{self.bucket_name}.s3"
-        if self.s3_hook.region_name:
-            url += f".{self.s3_hook.region_name}"
-        url += ".amazonaws.com"
-        if self.prefix:
-            url += f"/{self.prefix}"
-
-        return url
+    supports_versioning = False
