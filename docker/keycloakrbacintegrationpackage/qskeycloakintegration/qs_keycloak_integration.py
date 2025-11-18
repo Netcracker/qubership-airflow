94e24d673d247d2315020fdf363fdde0c3b8b0f6
Aakcht <aakcht@gmail.com>
2025-11-18 14:59:38 +0300

    add keycloak rbac integration package

--- docker/keycloakrbacintegrationpackage/qskeycloakintegration/qs_keycloak_integration.py
+++ docker/keycloakrbacintegrationpackage/qskeycloakintegration/qs_keycloak_integration.py
@@ -3,7 +3,9 @@ import os
 
 from typing import TYPE_CHECKING
 
-from airflow.providers.keycloak.auth_manager.keycloak_auth_manager import KeycloakAuthManager
+from airflow.providers.keycloak.auth_manager.keycloak_auth_manager import (
+    KeycloakAuthManager
+)
 from airflow.api_fastapi.common.types import MenuItem
 from airflow.providers.keycloak.auth_manager.user import KeycloakAuthManagerUser
 from airflow.providers.keycloak.auth_manager.resources import KeycloakResource
@@ -14,17 +16,23 @@ if TYPE_CHECKING:
 AIRFLOW_KEYCLOAK_ADMIN_ROLES = os.getenv("AIRFLOW_KEYCLOAK_ADMIN_ROLES", "airflow-role")
 airflow_keycloak_admin_roles_list = AIRFLOW_KEYCLOAK_ADMIN_ROLES.split(",")
 
+
 class QSRBACKeycloakAuthManager(KeycloakAuthManager):
+
+    @staticmethod
+    def _is_authorized_qs(user: KeycloakAuthManagerUser) -> bool:
+        token = user.access_token
+        me = jwt.decode(token, algorithms="RS256", options={"verify_signature": False})
+        for airflow_role in airflow_keycloak_admin_roles_list:
+            if airflow_role in me['realm_access']['roles']:
+                return True
+        return False
+
     def filter_authorized_menu_items(
         self, menu_items: list[MenuItem], *, user: KeycloakAuthManagerUser
     ) -> list[MenuItem]:
-        token = user.access_token
-        me = jwt.decode(
-            token, algorithms="RS256", options={"verify_signature": False}
-        )
-        for airflow_role in airflow_keycloak_admin_roles_list:
-            if airflow_role in me['realm_access']['roles']:
-                return menu_items
+        if self._is_authorized_qs(user):
+            return menu_items
         return []
 
     def _is_authorized(
@@ -36,13 +44,5 @@ class QSRBACKeycloakAuthManager(KeycloakAuthManager):
         resource_id: str | None = None,
         attributes: dict[str, str | None] | None = None,
     ) -> bool:
-
-        token = user.access_token
-        me = jwt.decode(
-            token, algorithms="RS256", options={"verify_signature": False}
-        )
-        for airflow_role in airflow_keycloak_admin_roles_list:
-            if airflow_role in me['realm_access']['roles']:
-                return True
-        return False
+        return self._is_authorized_qs(user)
 
