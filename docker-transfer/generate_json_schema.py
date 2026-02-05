c99be49c80be53346354010e645d8623cc2f967e
Aakcht <aakcht@gmail.com>
2026-02-05 11:07:17 +0300

    feat: chart update 3 iter

--- docker-transfer/generate_json_schema.py
+++ docker-transfer/generate_json_schema.py
@@ -8,7 +8,7 @@ from jsonschema import validate
 
 url = (
     "https://raw.githubusercontent.com/apache/airflow/"
-    "3314d54c3595f410a147790b7119392a0922268c/chart/values.schema.json"
+    "b1a6ebc6c5a14c48170de0d5472018905ab5edde/chart/values.schema.json"
 )
 global_params_to_keep = ["$schema", "description", "type", "definitions"]
 values_params_to_keep = [
