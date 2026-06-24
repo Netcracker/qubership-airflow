502899f3b8c02674579b8fa2b0d9eead54564142
Aakcht <aakcht@gmail.com>
2026-06-24 13:14:56 +0500

    fix: add test DAG to check username

--- tests_dags_image/dags/check_username_dag.py
+++ tests_dags_image/dags/check_username_dag.py
@@ -10,21 +10,23 @@ args = {
     "start_date": datetime.datetime(2025, 1, 1),
 }
 
-dag = DAG(dag_id="check_user_dag", default_args=args, schedule=None, tags=["check_user"])
+dag = DAG(
+    dag_id="check_user_dag", default_args=args, schedule=None, tags=["check_user"]
+)
 
 
 def check_user_1():
     try:
         print(f"USERNAME: {pwd.getpwuid(os.getuid())[0]}")
     except KeyError:
-        print(f"USERNAME NOT FOUND")
+        print("USERNAME NOT FOUND")
 
 
 def check_user_2():
     try:
         print(f"USERNAME: {pwd.getpwuid(os.getuid())[0]}")
     except KeyError:
-        print(f"USERNAME NOT FOUND")
+        print("USERNAME NOT FOUND")
 
 
 task1 = PythonOperator(
